d24ae4dce985ab27515027ff1a014167c965eec8
riwonn <102147448+riwonn@users.noreply.github.com>
2023-04-27 12:47:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@
     let fullImg = document.getElementById("imageBox");
     fullImg.src = smallImg.src;
 
-    if(document.getElementById('fullImage').style.display=='none'){
+    if(document.getElementById('fullImage').style.display==='none'){
       document.getElementById('fullImage').style.display='block';
      document.getElementById('fullImage').style.display='none';
   }
@@ -139,7 +139,7 @@
 
 
 function viewImg(){
-  if(document.getElementById('fullImage').style.display=='block'){
+  if(document.getElementById('fullImage').style.display==='block'){
       document.getElementById('fullImage').style.display='none';
   }else{
       document.getElementById('fullImage').style.display='block';
