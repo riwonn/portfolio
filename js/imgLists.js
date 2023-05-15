73cc66a3d7c997324a3d4efa86d9581f617ee827
riwon <everiwon0@gmail.com>
2023-05-15 23:13:47 +0900

    edit menu

--- js/imgLists.js
+++ js/imgLists.js
@@ -157,7 +157,7 @@ const images = [
     images.forEach(image => {
       if (category === image.category) {
         const li = document.createElement('li');
-        const img = document.createElement('img');
+        const img = document.creㅇㅂateElement('img');
         img.src = image.src;
         img.alt = image.alt;
         img.addEventListener('click', () => imgWide(img, image));
